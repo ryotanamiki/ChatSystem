1fe838505f38d63e4407d7fa72b53489d340b50e
r.namiki <aaasti555@gmail.com>
2023-08-29 17:23:12 +0900

    リファクタリング

--- main.js
+++ main.js
@@ -1,174 +1,167 @@
-window.onload = async function() {
+window.onload = async function () {
+    await initializeChat();
+};
+function createUserMessage(choice) {
+    const userMessage = createMessage('user', choice === 'A' ? 'ざっくり計算です' : 'しっかり計算です');
+    return userMessage;
+}
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function initializeChat() {
     const firstQuestion = document.getElementById("firstQuestion");
     const firstQuestion2 = document.getElementById("firstQuestion2");
 
     await showTemporaryMessage(firstQuestion, "・・・", 1000);
     firstQuestion.style.display = "block";
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await delay(1000);
+
     await showTemporaryMessage(firstQuestion2, "・・・", 1000);
     firstQuestion2.style.display = "block";
 
-    setTimeout(() => {
-        const box = document.getElementById("box");
-        box.style.display = "block";
-    }, 1000);
-};
+    await delay(1000);
 
-async function showTemporaryMessage(element, message, delay) {
+    const box = document.getElementById("box");
+    box.style.display = "block";
+}
+
+async function showTemporaryMessage(element, message, delayTime) {
     const chatContainer = document.querySelector('.chat');
-    const iconBox = document.querySelector('.icon-box');
-    const temporaryMessage = document.createElement('div');
-    temporaryMessage.className = 'message response';
-    temporaryMessage.textContent = message;
-    if (element.parentNode) {
-        element.parentNode.insertBefore(temporaryMessage, element);
-    }
+    const temporaryMessage = createMessage('response', message);
+    insertBeforeIfExists(temporaryMessage, element);
 
     await animateDots(temporaryMessage);
-    await new Promise(resolve => setTimeout(resolve, delay));
-        if (temporaryMessage.parentNode) {
-        temporaryMessage.parentNode.removeChild(temporaryMessage);
-    }
+    await delay(delayTime);
+
+    removeElementIfExists(temporaryMessage);
+}
+
+async function insertImage(element, imageUrl) {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => {
+            element.appendChild(image);
+            resolve();
+        };
+        image.onerror = () => {
+            reject();
+        };
+        image.src = imageUrl;
+    });
 }
 async function showResponse(choice) {
     const responseDiv = document.getElementById("response");
     const choiceAButton = document.getElementById("choiceA");
     const choiceBButton = document.getElementById("choiceB");
     const responseFinal = document.getElementById('final');
-    const chatContainer = document.querySelector('.chat');
-    const iconBox = document.querySelector('.icon-box');
     const choiceBox = document.getElementById('box');
 
-    choiceAButton.style.display = 'none';
-    choiceBButton.style.display = 'none';
-    choiceBox.style.display = 'none';
+    hideElements(choiceAButton, choiceBButton, choiceBox);
 
-    const userMessage = document.createElement('div');
-    userMessage.className = 'message user';
-    userMessage.textContent = (choice === 'A' ? 'ざっくり計算です' : 'しっかり計算です');
-    chatContainer.insertBefore(userMessage, responseDiv, responseFinal);
+    const userMessage = createUserMessage(choice);
+    insertBeforeIfExists(userMessage, responseDiv);
 
     if (choice === 'A') {
-        await new Promise(resolve => setTimeout(resolve, 500));
-        const iconDiv = document.createElement('div');
-        iconDiv.className = 'message icon';
-        chatContainer.insertBefore(iconDiv, responseDiv);
+        await delay(500);
+        const iconDiv = createMessage('icon');
+        insertBeforeIfExists(iconDiv, responseDiv);
+        await delay(1000);
 
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
-        const temporaryMessage = document.createElement('div');
-        temporaryMessage.className = 'message response icon';
-        temporaryMessage.textContent = '・・・';
-        chatContainer.insertBefore(temporaryMessage, responseDiv);
-
+        const temporaryMessage = createMessage('response icon', '・・・');
+        insertBeforeIfExists(temporaryMessage, responseDiv);
         await animateDots(temporaryMessage);
+        await delay(1000);
+        removeElementIfExists(temporaryMessage);
 
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        chatContainer.removeChild(temporaryMessage);
+        const systemMessage = createMessage('response', 'かしこまりました');
+        insertBeforeIfExists(systemMessage, responseDiv);
+        await delay(1000);
 
-        const systemMessage = document.createElement('div');
-        systemMessage.className = 'message response';
-        systemMessage.textContent = 'かしこまりました';
-        chatContainer.insertBefore(systemMessage, responseDiv);
-
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        const temporaryMessage2 = document.createElement('div');
-        temporaryMessage2.className = 'message response';
-        temporaryMessage2.textContent = '・・・';
-        chatContainer.insertBefore(temporaryMessage2, responseDiv);
-
+        const temporaryMessage2 = createMessage('response', '・・・');
+        insertBeforeIfExists(temporaryMessage2, responseDiv);
         await animateDots(temporaryMessage2);
-
-       await new Promise(resolve => setTimeout(resolve, 1000));
-        chatContainer.removeChild(temporaryMessage2);
+        await delay(1000);
+        removeElementIfExists(temporaryMessage2);
 
         responseDiv.innerHTML = "データをもとに、あなたの相場をざっくり計算します";
         responseDiv.style.display = 'block';
     } else if (choice === 'B') {
-        await new Promise(resolve => setTimeout(resolve, 500));
-        const iconDiv = document.createElement('div');
-        iconDiv.className = 'message icon';
-        chatContainer.insertBefore(iconDiv, responseDiv);
+        await delay(500);
+        const iconDiv = createMessage('icon');
+        insertBeforeIfExists(iconDiv, responseDiv);
+        await delay(1000);
 
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        const temporaryMessage = document.createElement('div');
-        temporaryMessage.className = 'message response icon';
-        temporaryMessage.textContent = '・・・';
-        chatContainer.insertBefore(temporaryMessage, responseDiv);
-
+        const temporaryMessage = createMessage('response icon', '・・・');
+        insertBeforeIfExists(temporaryMessage, responseDiv);
         await animateDots(temporaryMessage);
+        await delay(1000);
+        removeElementIfExists(temporaryMessage);
 
-       await new Promise(resolve => setTimeout(resolve, 1000));
-        chatContainer.removeChild(temporaryMessage);
+        const systemMessage = createMessage('response', 'かしこまりました');
+        insertBeforeIfExists(systemMessage, responseDiv);
+        await delay(1000);
 
-        const systemMessage = document.createElement('div');
-        systemMessage.className = 'message response';
-        systemMessage.textContent = 'かしこまりました';
-        chatContainer.insertBefore(systemMessage, responseDiv, responseFinal);
-
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        const temporaryMessage2 = document.createElement('div');
-        temporaryMessage2.className = 'message response';
-        temporaryMessage2.textContent = '・・・';
-        chatContainer.insertBefore(temporaryMessage2, responseDiv);
-
+        const temporaryMessage2 = createMessage('response', '・・・');
+        insertBeforeIfExists(temporaryMessage2, responseDiv);
         await animateDots(temporaryMessage2);
+        await delay(1000);
+        removeElementIfExists(temporaryMessage2);
 
-       await new Promise(resolve => setTimeout(resolve, 1000));
-        chatContainer.removeChild(temporaryMessage2);
+        const systemMessage2 = createMessage('response', 'お風呂のリフォーム相場は');
+        insertBeforeIfExists(systemMessage2, responseDiv);
+        await delay(1500);
 
-        const systemMessage2 = document.createElement('div');
-        systemMessage2.className = 'message response';
-        systemMessage2.textContent = 'お風呂のリフォーム相場は';
-        chatContainer.insertBefore(systemMessage2, responseDiv, responseFinal);
+        //グラフイメージ
+        for (let i = 1; i <= 4; i++) {
+        const imageDiv = document.getElementById(`image${i}`);
+        await insertImage(imageDiv, `images/img0${i}.png`);
+        await delay(1000);
+}
+        const iconDiv2 = createMessage('icon');
+        insertBeforeIfExists(iconDiv2, responseFinal);
+        await delay(1000);//
 
-        await new Promise(resolve => setTimeout(resolve, 1500));
-        const imageDiv = document.getElementById("image");
-        const image = document.createElement('img');
-        image.src = 'images/img01.png';
-        imageDiv.appendChild(image);
-
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        const imageDiv2 = document.getElementById("image2");
-        const image2 = document.createElement('img');
-        image2.src = 'images/img02.png';
-        imageDiv2.appendChild(image2);
-
-        await new Promise(resolve => setTimeout(resolve, 500));
-        const imageDiv3 = document.getElementById("image3");
-        const image3 = document.createElement('img');
-        image3.src = 'images/img03.png';
-        imageDiv3.appendChild(image3);
-
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        const imageDiv4 = document.getElementById("image4");
-        const image4 = document.createElement('img');
-        image4.src = 'images/img04.png';
-        imageDiv4.appendChild(image4);
-
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        const iconDiv2 = document.createElement('div');
-        iconDiv2.className = 'message icon';
-        chatContainer.insertBefore(iconDiv2, responseFinal);
-
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        const temporaryMessage3 = document.createElement('div');
-        temporaryMessage3.className = 'message response';
-        temporaryMessage3.textContent = '・・・';
-        chatContainer.insertBefore(temporaryMessage3, responseFinal);
-
+        const temporaryMessage3 = createMessage('response', '・・・');
+        insertBeforeIfExists(temporaryMessage3, responseFinal);
         await animateDots(temporaryMessage3);
-
-       await new Promise(resolve => setTimeout(resolve, 1000));
-        chatContainer.removeChild(temporaryMessage3);
+        await delay(1000);
+        removeElementIfExists(temporaryMessage3);
 
         responseFinal.innerHTML = "あなたの費用を、データをもとにしっかり計算します";
         responseFinal.style.display = 'block';
     }
 }
+function createMessage(className, content) {
+    const message = document.createElement('div');
+    message.className = `message ${className}`;
+    if (content) {
+        message.textContent = content;
+    }
+    return message;
+}
+
+function insertBeforeIfExists(newElement, referenceElement) {
+    if (referenceElement.parentNode) {
+        referenceElement.parentNode.insertBefore(newElement, referenceElement);
+    }
+}
+
+function removeElementIfExists(element) {
+    if (element.parentNode) {
+        element.parentNode.removeChild(element);
+    }
+}
+
+function hideElements(...elements) {
+    elements.forEach(element => {
+        element.style.display = 'none';
+    });
+}
+
 async function animateDots(element) {
-    const dots = element.textContent.split(""); // 点を一文字ずつに分割
-    element.textContent = ""; // 要素の内容をクリア
+    const dots = element.textContent.split("");
+    element.textContent = "";//ロードアニメーション
 
     for (let i = 0; i < dots.length; i++) {
         const dot = document.createElement("span");
@@ -182,6 +175,3 @@ async function animateDots(element) {
         }
     }
 }
-
-
-
