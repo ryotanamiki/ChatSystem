ee163569fd104738a24d47042187fc513e4a05c8
r.namiki <aaasti555@gmail.com>
2023-09-11 13:00:40 +0900

    ボタン選択とその後及び、地方選択挙動のリファクタリング

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ async function animateDots(element, ms = 500) {
 
     element.appendChild(dotsContainer);
 
-    await isDelay(ms);
+    await delay(ms);
 }
 
 //グラフイメージ
@@ -49,9 +49,6 @@ async function displayImages() {
 //ディレイ
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
-async function isDelay(ms) {
-    await delay(ms);
 }
 async function delayAll(ms = 1000) {
     await delay(ms);
@@ -224,126 +221,97 @@ async function displayButtons(buttonData) {
 
         //ボタンクリックイベント
         buttonContainer.addEventListener('click', async function (event) {
-            const targetBtn = event.target.closest('.button2');
-            const btnId = targetBtn.id;
+            const targetButton = event.target.closest('.button2');
+            if (targetButton) {
+                const btnId = targetButton.id;
+                const buttonText = targetButton.querySelector('span').textContent;
 
-            const userChoiceMap = {
-                'img05': 'ユニットバスです',
-                'img06': 'タイル貼りです',
-                'imgQ': 'わからないです',
-                'img07': '2畳未満です',
-                'img08': '2畳以上です',
-                'imgQ2': 'わからないです',
-                'img09': '広さ重視です',
-                'img10': '節水重視です',
-                'imgN': 'とくにないです',
-                'img11': 'はい',
-                'img12': '興味あり',
-                'imgN2': 'いいえ',
-                'img13': 'はい',
-                'img14': '興味あり',
-                'imgN3': 'いいえ',
-                'img15': 'はい',
-                'img16': '興味あり',
-                'imgN4': 'いいえ',
-                'img17': 'はい',
-                'img18': '興味あり',
-                'imgN5': 'いいえ',
-                'img19': 'はい',
-                'img20': '興味あり',
-                'imgN6': 'いいえ',
-                'img21': 'はい',
-                'img22': '興味あり',
-                'imgN7': 'いいえ',
-                'img23': 'はい',
-                'img24': '興味あり',
-                'imgN8': 'いいえ',
-            };
-            const userChoice = userChoiceMap[btnId];
-            displayUserMessage(userChoice);
-            buttonContainer.style.display = 'none';
+                // ボタンのテキストを表示
+                displayUserMessage(buttonText);
 
-            if (btnId === 'img05' || btnId === 'img06' || btnId === 'imgQ') {
-                await displaySetMessage('希望されるお風呂の大きさは、どのくらいですか？');
-                displayButtons([
-                    { id: 'img07', image: 'images/img07.png', text: '2畳未満' },
-                    { id: 'img08', image: 'images/img08.png', text: '2畳以上' },
-                    { id: 'imgQ2', image: 'images/imgQ.png', text: 'わからない' }
-                ]);
-            } else if (btnId === 'img07' || btnId === 'img08' || btnId === 'imgQ2') {
-                await displaySetMessage('浴槽まわりの希望をお伺いします。');
-                await displaySetMessage('湯煎に浸かる頻度が多い場合は、浴槽の形が重要です。');
-                await displaySetMessage('浴槽の形にこだわりはありますか？', false);
-                displayButtons([
-                    { id: 'img09', image: '', text: '広さ重視' },
-                    { id: 'img10', image: '', text: '節水重視' },
-                    { id: 'imgN', image: '', text: 'とくになし' }
-                ]);
-            } else if (btnId === 'img09' || btnId === 'img10' || btnId === 'imgN') {
-                await displaySetMessage('お湯の冷めにくい、保温効果のある浴槽を希望されますか？');
-                displayButtons([
-                    { id: 'img11', image: '', text: 'はい' },
-                    { id: 'img12', image: '', text: '興味あり' },
-                    { id: 'imgN2', image: '', text: 'いいえ' }
-                ]);
-            } else if (btnId === 'img11' || btnId === 'img12' || btnId === 'imgN2') {
-                await displaySetMessage('リラックス・マッサージ効果のある、バブルバス・ジェットバスをご希望されますか？');
-                displayButtons([
-                    { id: 'img13', image: '', text: 'はい' },
-                    { id: 'img14', image: '', text: '興味あり' },
-                    { id: 'imgN3', image: '', text: 'いいえ' }
-                ]);
-            } else if (btnId === 'img13' || btnId === 'img14' || btnId === 'imgN3') {
-                await displaySetMessage('お風呂に埋込み型のオーディオを設置すると、音の広がりがよく、また見た目もスッキリします。');
-                await displaySetMessage('お風呂にオーディオの設置を希望されますか？', false);
-                displayButtons([
-                    { id: 'img15', image: '', text: 'はい' },
-                    { id: 'img16', image: '', text: '興味あり' },
-                    { id: 'imgN4', image: '', text: 'いいえ' }
-                ]);
-            } else if (btnId === 'img15' || btnId === 'img16' || btnId === 'imgN4') {
-                await displaySetMessage('ゆったりお風呂に浸かりながら、最大24インチの大迫力の画面でテレビを楽しむこともできます。');
-                await displaySetMessage('お風呂にテレビの設置を希望されますか？', false);
-                displayButtons([
-                    { id: 'img17', image: '', text: 'はい' },
-                    { id: 'img18', image: '', text: '興味あり' },
-                    { id: 'imgN5', image: '', text: 'いいえ' }
-                ]);
-            } else if (btnId === 'img17' || btnId === 'img18' || btnId === 'imgN5') {
-                await displaySetMessage('設置する照明にこだわると、利用シーンに合わせて浴室の雰囲気を手軽にかえることができます');
-                await displaySetMessage('機能的な照明をご希望されますか？', false);
-                displayButtons([
-                    { id: 'img19', image: '', text: 'はい' },
-                    { id: 'img20', image: '', text: '興味あり' },
-                    { id: 'imgN6', image: '', text: 'いいえ' }
-                ]);
-            } else if (btnId === 'img19' || btnId === 'img20' || btnId === 'imgN6') {
-                await displaySetMessage('湯船に浸かる人が複数いたり、利用時間がバラバラな場合は、追い焚き機能が便利です。');
-                await displaySetMessage('追い焚き機能をご希望されますか？', false);
-                displayButtons([
-                    { id: 'img21', image: '', text: 'はい' },
-                    { id: 'img22', image: '', text: '興味あり' },
-                    { id: 'imgN7', image: '', text: 'いいえ' }
-                ]);
-            } else if (btnId === 'img21' || btnId === 'img22' || btnId === 'imgN7') {
-                await displaySetMessage('リビングの家族を呼び出したり会話ができるインターフォンの設置を希望しますか？');
-                displayButtons([
-                    { id: 'img23', image: '', text: 'はい' },
-                    { id: 'img24', image: '', text: '興味あり' },
-                    { id: 'imgN8', image: '', text: 'いいえ' }
-                ]);
-            } else if (btnId === 'img23' || btnId === 'img24' || btnId === 'imgN8') {
-                await displaySetMessage('物件の場所はどちらになりますか？');
-                displayButtons2([
-                    { id: 't1', text: '北海道・東北' },
-                    { id: 't2', text: '関東' },
-                    { id: 't3', text: '北陸・甲信越' },
-                    { id: 't4', text: '東海' },
-                    { id: 't5', text: '関西' },
-                    { id: 't6', text: '中国' },
-                    { id: 't7', text: '四国' },
-                    { id: 't8', text: '九州・沖縄' },
-                ]);
+                buttonContainer.style.display = 'none';
+                if (btnId === 'img05' || btnId === 'img06' || btnId === 'imgQ') {
+                    await displaySetMessage('希望されるお風呂の大きさは、どのくらいですか？');
+                    displayButtons([
+                        { id: 'img07', image: 'images/img07.png', text: '2畳未満' },
+                        { id: 'img08', image: 'images/img08.png', text: '2畳以上' },
+                        { id: 'imgQ2', image: 'images/imgQ.png', text: 'わからない' }
+                    ]);
+                } else if (btnId === 'img07' || btnId === 'img08' || btnId === 'imgQ2') {
+                    await displaySetMessage('浴槽まわりの希望をお伺いします。');
+                    await displaySetMessage('湯煎に浸かる頻度が多い場合は、浴槽の形が重要です。');
+                    await displaySetMessage('浴槽の形にこだわりはありますか？', false);
+                    displayButtons([
+                        { id: 'img09', image: '', text: '広さ重視' },
+                        { id: 'img10', image: '', text: '節水重視' },
+                        { id: 'imgN', image: '', text: 'とくになし' }
+                    ]);
+                } else if (btnId === 'img09' || btnId === 'img10' || btnId === 'imgN') {
+                    await displaySetMessage('お湯の冷めにくい、保温効果のある浴槽を希望されますか？');
+                    displayButtons([
+                        { id: 'img11', image: '', text: 'はい' },
+                        { id: 'img12', image: '', text: '興味あり' },
+                        { id: 'imgN2', image: '', text: 'いいえ' }
+                    ]);
+                } else if (btnId === 'img11' || btnId === 'img12' || btnId === 'imgN2') {
+                    await displaySetMessage('リラックス・マッサージ効果のある、バブルバス・ジェットバスをご希望されますか？');
+                    displayButtons([
+                        { id: 'img13', image: '', text: 'はい' },
+                        { id: 'img14', image: '', text: '興味あり' },
+                        { id: 'imgN3', image: '', text: 'いいえ' }
+                    ]);
+                } else if (btnId === 'img13' || btnId === 'img14' || btnId === 'imgN3') {
+                    await displaySetMessage('お風呂に埋込み型のオーディオを設置すると、音の広がりがよく、また見た目もスッキリします。');
+                    await displaySetMessage('お風呂にオーディオの設置を希望されますか？', false);
+                    displayButtons([
+                        { id: 'img15', image: '', text: 'はい' },
+                        { id: 'img16', image: '', text: '興味あり' },
+                        { id: 'imgN4', image: '', text: 'いいえ' }
+                    ]);
+                } else if (btnId === 'img15' || btnId === 'img16' || btnId === 'imgN4') {
+                    await displaySetMessage('ゆったりお風呂に浸かりながら、最大24インチの大迫力の画面でテレビを楽しむこともできます。');
+                    await displaySetMessage('お風呂にテレビの設置を希望されますか？', false);
+                    displayButtons([
+                        { id: 'img17', image: '', text: 'はい' },
+                        { id: 'img18', image: '', text: '興味あり' },
+                        { id: 'imgN5', image: '', text: 'いいえ' }
+                    ]);
+                } else if (btnId === 'img17' || btnId === 'img18' || btnId === 'imgN5') {
+                    await displaySetMessage('設置する照明にこだわると、利用シーンに合わせて浴室の雰囲気を手軽にかえることができます');
+                    await displaySetMessage('機能的な照明をご希望されますか？', false);
+                    displayButtons([
+                        { id: 'img19', image: '', text: 'はい' },
+                        { id: 'img20', image: '', text: '興味あり' },
+                        { id: 'imgN6', image: '', text: 'いいえ' }
+                    ]);
+                } else if (btnId === 'img19' || btnId === 'img20' || btnId === 'imgN6') {
+                    await displaySetMessage('湯船に浸かる人が複数いたり、利用時間がバラバラな場合は、追い焚き機能が便利です。');
+                    await displaySetMessage('追い焚き機能をご希望されますか？', false);
+                    displayButtons([
+                        { id: 'img21', image: '', text: 'はい' },
+                        { id: 'img22', image: '', text: '興味あり' },
+                        { id: 'imgN7', image: '', text: 'いいえ' }
+                    ]);
+                } else if (btnId === 'img21' || btnId === 'img22' || btnId === 'imgN7') {
+                    await displaySetMessage('リビングの家族を呼び出したり会話ができるインターフォンの設置を希望しますか？');
+                    displayButtons([
+                        { id: 'img23', image: '', text: 'はい' },
+                        { id: 'img24', image: '', text: '興味あり' },
+                        { id: 'imgN8', image: '', text: 'いいえ' }
+                    ]);
+                } else if (btnId === 'img23' || btnId === 'img24' || btnId === 'imgN8') {
+                    await displaySetMessage('物件の場所はどちらになりますか？');
+                    displayButtons2([
+                        { id: 't1', text: '北海道・東北' },
+                        { id: 't2', text: '関東' },
+                        { id: 't3', text: '北陸・甲信越' },
+                        { id: 't4', text: '東海' },
+                        { id: 't5', text: '関西' },
+                        { id: 't6', text: '中国' },
+                        { id: 't7', text: '四国' },
+                        { id: 't8', text: '九州・沖縄' },
+                    ]);
+                }
             }
         });
     }
@@ -447,58 +415,15 @@ async function displayButtons(buttonData) {
                 { id: 't61', text: '沖縄県' }
             ],
         };
-    }
-//都道府県選択後のユーザーメッセージ
-    function getDisplayText(btnId) {
-        const textMapping = {
-            't9': '北海道です',
-            't10': '青森県です',
-            't11': '岩手県です',
-            't12': '宮城県です',
-            't13': '秋田県です',
-            't14': '山形県です',
-            't15': '福島県です',
-            't16': '茨城県です',
-            't17': '栃木県です',
-            't18': '群馬県です',
-            't19': '埼玉県です',
-            't20': '千葉県です',
-            't21': '東京都です',
-            't22': '神奈川県です',
-            't30': '富山県です',
-            't31': '石川県です',
-            't32': '福井県です',
-            't33': '山梨県です',
-            't34': '長野県です',
-            't35': '新潟県です',
-            't36': '静岡県です',
-            't37': '愛知県です',
-            't38': '三重県です',
-            't39': '岐阜県です',
-            't40': '大阪府です',
-            't41': '京都府です',
-            't42': '兵庫県です',
-            't43': '奈良県です',
-            't44': '滋賀県です',
-            't45': '和歌山県です',
-            't46': '鳥取県です',
-            't47': '島根県です',
-            't48': '岡山県です',
-            't49': '広島県です',
-            't50': '山口県です',
-            't51': '香川県です',
-            't52': '徳島県です',
-            't53': '高知県です',
-            't54': '愛媛県です',
-            't55': '福岡県です',
-            't56': '佐賀県です',
-            't57': '大分県です',
-            't58': '熊本県です',
-            't59': '宮崎県です',
-            't60': '鹿児島県です',
-            't61': '沖縄県です'
-        };
 
-        return textMapping[btnId] || '';
+function getDisplayText(btnId) {
+    for (const key in buttonTextMapping) {
+        const mappings = buttonTextMapping[key];
+        const mapping = mappings.find(mapping => mapping.id === btnId);
+        if (mapping) {
+            return mapping.text;
+        }
+    }
+}
     }
 });
